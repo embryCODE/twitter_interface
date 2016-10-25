33c8c8406ece033d6109d73aed76a377ff812834
Mason Embry <mason@embrycode.com>
2016-10-25 10:48:45 -0500

    Some JSDoc comments added.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,22 @@
 'use strict';
 
-var Twit = require('twit');
-var keys = require('./keys');
+/**
+ * @file Portfolio Project 7 for Techdegree. Build a Twitter Interface.
+ * {@link https://teamtreehouse.com/projects/build-a-twitter-interface}
+ * @author Mason Embry <mason@embrycode.com>
+ */
 
+/**
+ * Twitter interface module for Treehouse Techdegree.
+ * @module app
+ */
+
+
+var Twit = require('twit'),
+  keys = require('./keys');
+
+/**
+ * Create twit object.
+ * @param {object} keys OAuth keys from separate file.
+ */
 var twit = new Twit(keys);

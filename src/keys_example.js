9448946c312b96eba5da5a8d732b233214b54ccf
Mason Embry <mason@embrycode.com>
2016-10-25 10:39:19 -0500

    Scaffolding and templates are in place but not wired up.

---
+++ src/keys_example.js
@@ -0,0 +1,12 @@
+/**
+ * NOTE: Rename this file as "keys.js" and insert your keys below.
+ */
+
+'use strict';
+
+module.exports = {
+  consumer_key: '',
+  consumer_secret: '',
+  access_token: '',
+  access_token_secret: ''
+};

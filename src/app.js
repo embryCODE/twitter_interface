efe726642a6b071bdc8c76b19cef897204e0ad34
Mason Embry <mason@embrycode.com>
2016-10-25 10:08:11 -0500

    Setup jshint and eslint.

--- src/app.js
+++ src/app.js
@@ -4,7 +4,3 @@ var Twit = require('twit');
 var keys = require('./keys');
 
 var twit = new Twit(keys);
-
-twit.post('statuses/destroy/:id', { id: '790928195377885185' }, function (err, data, response) {
-  console.log(data);
-});

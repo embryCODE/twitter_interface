8345957f33b556b2acac6ef0fc9ed585899f7caa
Mason Embry <mason@embrycode.com>
2016-10-26 11:42:26 -0500

    Timestamps added to client-side JS.

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,6 +1,9 @@
 (function() {
   'use strict';
 
-  $('.app-tweet-timestamp').html(timestamp);
-
+  $('.app--tweet--timestamp, .app--message--timestamp').each(function() {
+    var thisElementTime = new Date(this.innerHTML).toISOString();
+    var timestamp = $.timeago(thisElementTime);
+    this.innerHTML = timestamp;
+  });
 })();

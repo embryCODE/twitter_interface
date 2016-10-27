0bd6f1c81cfbf360472d86f200d21f1a35a0d076
Mason Embry <mason@embrycode.com>
2016-10-27 10:34:56 -0400

    Cleaned up.

--- src/twitter.js
+++ src/twitter.js
@@ -34,46 +34,46 @@ function getTwitterData(req, res, next) {
         function(err, data, response) {
           var user_timeline = data;
           twitter.get('friends/list', {
-            screen_name: config.screen_name,
-            count: 5
-          }, function(err, data, response) {
-            var friends = data;
-            twitter.get('direct_messages/sent', {
+              screen_name: config.screen_name,
               count: 5
-            }, function(err, data, response) {
-              var direct_messages_sent = data;
-              twitter.get('direct_messages', {
-                count: 5
-              }, function(err, data, response) {
-                var direct_messages_received = data;
-                // Concats the two arrays together.
-                var direct_messages_array = direct_messages_sent.concat(direct_messages_received);
+            },
+            function(err, data, response) {
+              var friends = data;
+              twitter.get('direct_messages/sent', {
+                  count: 5
+                },
+                function(err, data, response) {
+                  var direct_messages_sent = data;
+                  twitter.get('direct_messages', {
+                      count: 5
+                    },
+                    function(err, data, response) {
+                      var direct_messages_received = data;
+                      var direct_messages_array = direct_messages_sent.concat(direct_messages_received);
+                      direct_messages_array.sort(function(a, b) {
+                        return parseFloat(a.id) - parseFloat(b.id);
+                      });
+                      direct_messages_array.reverse();
+                      direct_messages_array.splice(-5, 5);
 
-                // Sorts messages by id, which should make them oldest to newest in the array.
-                direct_messages_array.sort(function(a, b) {
-                  return parseFloat(a.id) - parseFloat(b.id);
-                });
-
-                // Reverse order so newest messages are first in the array.
-                direct_messages_array.reverse();
-
-                // Reduce length to only five messages.
-                direct_messages_array.splice(-5, 5);
-
-                res.render('index', {
-                  title: 'Twitter Client',
-                  user: user,
-                  user_timeline: user_timeline,
-                  friends: friends,
-                  direct_messages_array: direct_messages_array,
-                  config: config
-                });
-
-              });
-            });
-          });
-        });
-    });
+                      res.render('index', {
+                        title: 'Twitter Client',
+                        user: user,
+                        user_timeline: user_timeline,
+                        friends: friends,
+                        direct_messages_array: direct_messages_array,
+                        config: config
+                      });
+                    }
+                  );
+                }
+              );
+            }
+          );
+        }
+      );
+    }
+  );
 }
 
 exports.getTwitterData = getTwitterData;

5caf9821a01c5e2f28f66e35ba04804259d419c3
Mason Embry <mason@embrycode.com>
2016-10-28 14:02:04 -0400

    Typo

--- src/twitter.js
+++ src/twitter.js
@@ -108,14 +108,14 @@ function getTwitterData(req, res, next) {
  * Posts a tweet to twitter.
  */
 function postTweet(req, res, next) {
-  twitter.post('statuses/update', {
+  twitter.post('ASDFstatuses/update', {
     status: req.body.tweet
   }, function(err, data, response) {
     if (!err) {
       // res.send('Your tweet was tweeted!');
       getTwitterData(req, res, next);
     } else {
-      //res.send('Sorry, there was an error.');
+      // res.send('Sorry, there was an error.');
       return next(err);
     }
   });
